0043177a84c78282bd47a5d70bf32415a1f60450
J Navaneetha Krishnaa <navaneethajayaprakash45@gmail.com>
2026-05-06 22:30:51 +0530

    Add: n-queens - submission-4

---
+++ Data Structures & Algorithms/n-queens/submission-4.py
@@ -0,0 +1,30 @@
+class Solution:
+    def solveNQueens(self, n: int) -> List[List[str]]:
+        res = []
+        posDiag = set()
+        negDiag = set()
+        cols = set()
+        board = [["."]*n for _ in range(n)]
+
+        def dfs(r):
+            if r >= n:
+                copy = ["".join(row) for row in board]
+                res.append(copy)
+                return 
+
+            for c in range(n):
+                if c in cols or r + c in posDiag or r - c in negDiag:
+                    continue 
+                
+                board[r][c] = 'Q'
+                posDiag.add(r+c)
+                negDiag.add(r-c)
+                cols.add(c)
+                dfs(r + 1)
+                board[r][c] = "."
+                posDiag.remove(r+c)
+                negDiag.remove(r-c)
+                cols.remove(c)
+
+        dfs(0)
+        return res 
