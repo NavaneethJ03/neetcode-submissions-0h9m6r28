207b99f7b40d1afa7325a68470066e6955dfb54f
J Navaneetha Krishnaa <navaneethajayaprakash45@gmail.com>
2026-09-14 16:55:46 +0530

    Add: partition-labels - submission-6

---
+++ Data Structures & Algorithms/partition-labels/submission-6.py
@@ -0,0 +1,17 @@
+class Solution:
+    def partitionLabels(self, s: str) -> List[int]:
+        res = []
+        lastSeen = {}
+        for i , c in enumerate(s):
+            lastSeen[c] = i
+        
+        l = 0 
+        dist = 0 
+        for r , c in enumerate(s):
+            dist = max(dist , lastSeen[c])
+            if r == dist:
+                res.append(r - l + 1)
+                l = r + 1 
+                # dist = 0 
+
+        return res
