d5dd51180cf111488e08218200e05509f5f081cf
J Navaneetha Krishnaa <navaneethajayaprakash45@gmail.com>
2026-09-20 09:57:22 +0530

    Add: clone-graph - submission-11

---
+++ Data Structures & Algorithms/clone-graph/submission-11.py
@@ -0,0 +1,28 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+
+class Solution:
+    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+        hmap = {None : None}
+
+        def dfs(node):
+            if not node:
+                return None 
+
+            if node in hmap:
+                return hmap[node]
+            
+            copy = Node(node.val)
+            hmap[node] = copy
+
+            for neinode in node.neighbors:
+                copy.neighbors.append(dfs(neinode))
+
+            return hmap[node]
+
+        return dfs(node)
